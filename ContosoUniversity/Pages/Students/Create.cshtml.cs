d22fa397a67e62ea09ea6882df2dbec3bd5a3d02
LeeTrent <Lee.Trent.1@gmail.com>
2018-04-23 09:38:19 -0500

    Changes scaffolding generated create OnPostAsyc method to use 'TryUpdateModelAsync<Student> in Create.cshtml.cs

--- ContosoUniversity/Pages/Students/Create.cshtml.cs
+++ ContosoUniversity/Pages/Students/Create.cshtml.cs
@@ -27,6 +27,38 @@ namespace ContosoUniversity.Pages.Students
         [BindProperty]
         public Student Student { get; set; }
 
+        ///////////////////////////////////////////////////////
+        // Original scaffolding code
+        ///////////////////////////////////////////////////////
+        // public async Task<IActionResult> OnPostAsync()
+        // {
+        //     if (!ModelState.IsValid)
+        //     {
+        //         return Page();
+        //     }
+
+        //     _context.Students.Add(Student);
+        //     await _context.SaveChangesAsync();
+
+        //     return RedirectToPage("./Index");
+        // }
+        ///////////////////////////////////////////////////////
+        
+        /**********************************************************************************************
+            TryUpdateModelAsync<Student> tries to update the emptyStudent object using the posted
+            form values from the PageContext property in the PageModel. 
+            
+            TryUpdateModelAsync only updates the properties listed
+            (s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate).
+            
+            The second argument ("student", // Prefix) is the prefix uses to look up values.
+            It's not case sensitive.
+            
+            The posted form values are converted to the types in the Student model using model binding.
+            
+            Using TryUpdateModel to update fields with posted values is a security best practice
+            because it prevents overposting.
+        ************************************************************************************************/
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
@@ -34,10 +66,20 @@ namespace ContosoUniversity.Pages.Students
                 return Page();
             }
 
-            _context.Students.Add(Student);
-            await _context.SaveChangesAsync();
+            var emptyStudent = new Student();
 
-            return RedirectToPage("./Index");
+            if (await TryUpdateModelAsync<Student>(
+                emptyStudent,
+                "student",   // Prefix for form value.
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+            {
+                _context.Students.Add(emptyStudent);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            return null;
         }
+
     }
 }
